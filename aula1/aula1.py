7a6c0eeac8a4c6961bd70a2e1fdb935d3ea36584
= <=>
2026-09-15 10:57:18 +0100

    vamoquivamo

---
+++ aula1/aula1.py
@@ -0,0 +1,21 @@
+import numpy as np
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+
+# Ler o ficheiro
+fs, audio = wavfile.read("audio2.wav")
+
+print("Frequência de amostragem:", fs, "Hz")
+print("Número de amostras:", len(audio))
+print("Duração:", len(audio) / fs, "segundos")
+
+# Criar eixo temporal
+t = np.arange(len(audio)) / fs
+
+# Plot
+plt.plot(t, audio)
+plt.xlabel("Tempo [s]")
+plt.ylabel("Amplitude")
+plt.title("plot tempo")
+plt.grid()
+plt.show()
